692a04eac24ada672d34aeada4f5e15f80753406
SK NADIM <128034110+Sknadim6297@users.noreply.github.com>
2024-08-15 00:03:43 +0530

    hello

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 
  export const connetDb=async () =>{
-    await mongoose.connect('mongodb+srv://sknadim6297:Nadeem@nadeem.cl2gf.mongodb.net/?retryWrites=true&w=majority&appName=Nadeem',{
+    await mongoose.connect('mongodb+srv://sknadim6297:i65KtFozDWRdkk04@mern-food-app.xlhmh1m.mongodb.net/',{
     });
     console.log('Database connected');
 }   
